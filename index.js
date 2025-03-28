208a8f525075e5d64f9deeef412d128744d5697d
MinsUR4 <164434993+MinsUR4@users.noreply.github.com>
2025-03-28 15:47:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+(() => {
+    const style = document.createElement("style");
+    style.textContent = `
+        body, html {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    `;
+    document.head.appendChild(style);
+
+    const iframe = document.createElement("iframe");
+    iframe.src = "https://elixirr-ca15e8eff6a0.herokuapp.com/";
+    iframe.allowFullscreen = true;
+  
+    document.body.appendChild(iframe);
+})();
