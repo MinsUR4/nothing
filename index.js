4cf84e6fb68cc775764e14db74a72558d0f47e26
MinsUR4 <164434993+MinsUR4@users.noreply.github.com>
2025-03-30 21:22:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (() => {
     const style = document.createElement("style");
-    style.textContent = 
+    style.textContent = `
         body, html {
             margin: 0;
             padding: 0;
@@ -13,12 +13,12 @@
             height: 100%;
             border: none;
         }
-    ;
+    `;
     document.head.appendChild(style);
 
     const iframe = document.createElement("iframe");
     iframe.src = "https://elixirr-ca15e8eff6a0.herokuapp.com/";
-    iframe.allowFullscreen = true;
-  
+    iframe.setAttribute("allowFullscreen", "true");
+
     document.body.appendChild(iframe);
 })();
