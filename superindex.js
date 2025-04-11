7aeeb95c4cc8ca3686c2520bf5a3e6aa24fbf851
MinsUR4 <164434993+MinsUR4@users.noreply.github.com>
2025-04-10 20:09:38 -0400

    Update superindex.js

--- superindex.js
+++ superindex.js
@@ -17,7 +17,7 @@
     document.head.appendChild(style);
 
     const iframe = document.createElement("iframe");
-    iframe.src = "https://jubilant-halibut-wrvx4479v49vc9qxq-8080.app.github.dev/";
+    iframe.src = "https://elixirr-ca15e8eff6a0.herokuapp.com/";
     iframe.setAttribute("allowFullscreen", "true");
 
     document.body.appendChild(iframe);
